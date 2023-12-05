e273c4b374d5d9e106a15d92aeb650dc3a7e581d
unknown <dileeproyal4243@gmail.com>
2023-12-05 22:00:39 +0530

    solved

--- Assignments/Week-1/easy/expenditure-analysis.js
+++ Assignments/Week-1/easy/expenditure-analysis.js
@@ -6,7 +6,34 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  // array to return the result
+  let output=[];
+
+  if(transactions.length==0) return [];
+  
+  // storing a intial object with unique category
+  output.push({"category":transactions[0]["category"],"totalSpent":transactions[0]["price"]});
+  
+  // traversing each object of transactions array
+  for(let i=1;i<transactions.length;i++){
+      let price=transactions[i]["price"];
+      let category=transactions[i]["category"];
+      
+      categoryAlreadyExist=false;
+
+      // traversing each object of ouput array to check if category already exists or not.
+      for(let j=0;j<output.length;j++){ 
+        if(output[j]["category"]===category){
+          output[j]["totalSpent"]+=price;
+          categoryAlreadyExist=true;
+          break;
+        }
+      }
+
+      if(!categoryAlreadyExist) output.push({"category":category,"totalSpent":price});
+  }
+  return output;
 }
 
 module.exports = calculateTotalSpentByCategory;
